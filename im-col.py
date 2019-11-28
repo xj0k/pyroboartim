092e94c0aa51015ddef11ddee560e3189315f259
rayson <rayson.xu@gmail.com>
2019-11-28 09:37:47 +0800

    Add frames

--- im-col.py
+++ im-col.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import PySimpleGUIQt as sg
+import PySimpleGUI as sg
 import sleekxmpp
+import time
+
+gui_debug = True
 
 window = None
 
@@ -11,35 +14,46 @@ password = "1234!@#$"
 server = ("chatserver.space", 5222)
 gClient = sleekxmpp.ClientXMPP(jid, password)
 
-def update_text(msg):
+#mto = "xj0k@chatserver.space"
+mto = 'xj0k1234@trillian.im'
+
+txt_wid = 16
+size_i = (txt_wid,1)
+size_f = (txt_wid,26)
+size_b = (6,1)
+
+def update_recv_text(msg):
     txt = window.Element('text')
-    t = txt.Get()
-    if t:
-        v = txt.Get() + '\n' +msg
-        print("current message:%s" % t)
-    else:
-        v = msg
-    txt.Update(value=v)
+    m =  "接收:"+msg
+    tc = 'blue'
+    v = '\n'+ m
+    txt.Update(value=v,append=True,text_color=tc)
 
-
-def show_message(msg):
-    window.Element('recv').Update(value=msg)
+def update_send_text(msg):
+    txt = window.Element('text')
+    #m = 'send>' + msg
+    m =  "发送："+ msg
+    tc = 'blue'
+    v = '\n'+ m
+    txt.Update(value=v,append=True,text_color=tc)
 
 def send_msg(msg):
-    return
-    mto = "xj0k@chatserver.space"
-    mto = 'xj0k1234@trillian.im'
+    if gui_debug:
+        return
     gClient.send_message(mto,msg)
 
-def recv_message( msg):
+def recv_message(msg):
     t = msg['body']
-    show_message(t)
-    #update_text(t)
+    update_recv_text(t)
 
 def im_logout():
+    if gui_debug:
+        return
     gClient.disconnect(wait=True)
 
 def im_login():
+    if gui_debug:
+        return
     gClient.add_event_handler("message", recv_message)
     if gClient.connect(server):
         print("Connected, start processing messages.")
@@ -50,21 +64,29 @@ def im_login():
         print("Unable to connect.")
 
 
-col_chat = [
-    [sg.Multiline(key='text',size=(16,6))],
-    [sg.InputText("message here to send",key='message'), sg.Button('send', key='send')]
-]
-
-col_login = [
+frm_login = [
     [sg.Image(filename="/users/netbox-wangdong/downloads/logo-srap.png")],
     [sg.Text("Please login")],
-    [sg.InputText(key='id', default_text='user id')],
-    [sg.InputText(key='password')],
-    [sg.Button("login", key='start'), sg.Button('exit', key='exit')]
+    [sg.InputText(key='id', default_text='user id',size=size_i)],
+    [sg.InputText(key='password',size=size_i)],
+    [sg.Button("login", key='start',size=size_b), sg.Button('exit', key='exit',size=size_b)]
+]
+
+frd = ['1xxxx','2xxxx','3xxxx']
+frm_friends = [
+    [sg.Listbox(values=frd,size=(txt_wid,14))]
+]
+
+frm_text = [
+    [sg.Multiline(key='text',size=(txt_wid,14),disabled=True)],
+    [sg.InputText("",key='message',size=size_i) ],
+    [sg.Button('send', key='send',size=size_b), sg.Button('back', key='back',size=size_b)]
 ]
 
 layout = [
-    [sg.Column(col_login),sg.Column(col_chat)]
+    [sg.Frame('登录',key='frm_login',layout=frm_login,size=size_f),
+        sg.Frame('friends',key='frm_friends',layout=frm_friends,visible=True,size=size_f),
+        sg.Frame('消息',key='frm_text',layout=frm_text,visible=True,size=size_f)]
 ]
 
 window = sg.Window("roboartIM",layout)
@@ -79,12 +101,24 @@ while True:
     if event == 'start':
         print("will start chat process.")
         im_login()
+        window.Element('frm_login').Update(visible=False)
+        window.Element('frm_text').Update(value=mto,visible=True)
 
     if event == 'send':
         msg = window.Element('message').Get()
         print("sending message:%s" % msg )
         send_msg(msg)
         window.Element('message').Update(value='')
-        update_text(str(msg))
+        update_send_text(str(msg))
+
+    if event == 'recv_test':
+        msg = window.Element('recv').Get()
+        window.Element('recv').Update(value='')
+        update_recv_text(str(msg))
+
+    if event == 'back':
+        window.Element('frm_login').Update(visible=True)
+        window.Element('frm_text').Update(visible=False)
+
 
 
